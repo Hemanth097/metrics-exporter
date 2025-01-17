5cd35305374ade7ca05bc1cfa301b0fbebd2aec1
Hemanth Vudumula <vudumulahemanth@gmail.com>
2025-01-18 00:18:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,28 +17,28 @@ def fetch_cpu_utilization():
     cpu_percent = psutil.cpu_percent(interval=1)
     cpu_utilization.set(cpu_percent)
 
-def fetch_tpu_utilization():
-    # Get TPU usage from Cloud Monitoring API
-    interval = monitoring_v3.TimeInterval({
-        "end_time": {"seconds": int(time.time())},
-        "start_time": {"seconds": int(time.time()) - 60},  # Last 5 minutes
-    })
-    results = client.list_time_series(
-        request={
-            "name": project_name,
-            "filter": 'metric.type="cloud_tpu.googleapis.com/tpu/utilization"',
-            "interval": interval,
-            "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
-        }
-    )
-    for result in results:
-        for point in result.points:
-            utilization = point.value.double_value * 100  # Convert to percentage
-            tpu_utilization.set(utilization)
+# def fetch_tpu_utilization():
+#     # Get TPU usage from Cloud Monitoring API
+#     interval = monitoring_v3.TimeInterval({
+#         "end_time": {"seconds": int(time.time())},
+#         "start_time": {"seconds": int(time.time()) - 60},  # Last 5 minutes
+#     })
+#     results = client.list_time_series(
+#         request={
+#             "name": project_name,
+#             "filter": 'metric.type="cloud_tpu.googleapis.com/tpu/utilization"',
+#             "interval": interval,
+#             "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
+#         }
+#     )
+#     for result in results:
+#         for point in result.points:
+#             utilization = point.value.double_value * 100  # Convert to percentage
+#             tpu_utilization.set(utilization)
 
 if __name__ == "__main__":
     start_http_server(9102)  # Expose metrics on port 9102
     while True:
         fetch_cpu_utilization()
-        fetch_tpu_utilization()
+        # fetch_tpu_utilization()
         time.sleep(60)  # Sleep for 1 minute before fetching again
