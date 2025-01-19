f09ec7f0f56a5a271f5faa89043309fd5b5317f1
Hemanth Vudumula <vudumulahemanth@gmail.com>
2025-01-19 18:19:01 +0530

    Update tpu_exporter_start.sh

--- tpu_exporter_start.sh
+++ tpu_exporter_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+sudo apt update
+sudo apt install python3
+sudo apt install python3-pip
+python3 -m venv venv
 source venv/bin/activate
 
 pip install  prometheus_client google-cloud-monitoring
