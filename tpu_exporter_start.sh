4c40214bce6e2044c232535f5170c4122233499b
Hemanth Vudumula <vudumulahemanth@gmail.com>
2025-01-20 10:11:30 +0530

    Update tpu_exporter_start.sh

--- tpu_exporter_start.sh
+++ tpu_exporter_start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-sudo apt update
-sudo apt install python3
-sudo apt install python3-venv
-sudo apt install python3-pip
-python3 -m venv venv
+
+python -m venv /opt/tpu_exporter/venv
 source venv/bin/activate
 echo "Created exporter"
 sudo chmod -R 777 /opt/tpu_exporter
 echo "Chmod exporter"
 sudo chmod -R 777 /opt/tpu_exporter/venv
 echo "chmod venv"
-/opt/tpu_exporter/venv/bin/pip3 install  prometheus_client google-cloud-monitoring
+/opt/tpu_exporter/venv/bin/pip install  prometheus_client google-cloud-monitoring
 echo "installation done"
-/opt/tpu_exporter/venv/bin/python3 /opt/tpu_exporter/tpu_exporter.py --variable "$1"
+/opt/tpu_exporter/venv/bin/python /opt/tpu_exporter/tpu_exporter.py --variable "$1"
 echo "running"
